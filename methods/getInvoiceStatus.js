9bbc7392f4b9c9dcc0113743038f2c3297998d4d
velwix10 <velwix.10@gmail.com>
2026-07-23 16:07:11 +0500

    Create getInvoiceStatus.js

---
+++ methods/getInvoiceStatus.js
@@ -0,0 +1,19 @@
+module.exports = async function getInvoiceStatus(client, uuid) {
+  const fetchApi = globalThis.fetch;
+
+  if (!fetchApi) {
+    throw new Error("Fetch API ushbu muhitda qo'llab-quvvatlanmaydi.");
+  }
+
+  const id = typeof uuid === 'object' ? (uuid.uuid || uuid.id) : uuid;
+
+  const res = await fetchApi(`${client.baseUrl}/api/v1/invoice/${id}`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${client.token}`,
+      'Accept': 'application/json'
+    }
+  });
+
+  return await res.json();
+};
